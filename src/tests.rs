eb46f98a36ce9b6207135b240ecda148b9172e8c
Jonathan Brown <jbrown@acuity.social>
2021-06-15 13:18:26 +0700

    Test timeout_sell()

--- src/tests.rs
+++ src/tests.rs
@@ -201,3 +201,100 @@ fn unlock_sell() {
 
 	});
 }
+
+#[test]
+fn timeout_sell_order_control_wrong_order_id() {
+    new_test_ext().execute_with(|| {
+        let price: u128 = 5;
+        let secret = [0; 32];
+        let hashed_secret: [u8; 32] = keccak_256(&secret);
+        let _now = <pallet_timestamp::Pallet<Test>>::get();
+		assert_ok!(AcuityAtomicSwap::add_to_order(Origin::signed(A), price, 50));
+        assert_ok!(AcuityAtomicSwap::lock_sell(Origin::signed(A), price, hashed_secret, _now, 10));
+        assert_ok!(AcuityAtomicSwap::timeout_sell(Origin::signed(A), price, hashed_secret));
+	});
+}
+
+#[test]
+fn timeout_sell_order_fail_wrong_order_id() {
+    new_test_ext().execute_with(|| {
+        let price: u128 = 5;
+        let secret = [0; 32];
+        let hashed_secret: [u8; 32] = keccak_256(&secret);
+        let _now = <pallet_timestamp::Pallet<Test>>::get();
+		assert_ok!(AcuityAtomicSwap::add_to_order(Origin::signed(A), price, 50));
+        assert_ok!(AcuityAtomicSwap::lock_sell(Origin::signed(A), price, hashed_secret, _now, 10));
+		assert_noop!(
+            AcuityAtomicSwap::timeout_sell(Origin::signed(A), price - 1, hashed_secret),
+			Error::<Test>::WrongOrderId
+		);
+	});
+}
+
+#[test]
+fn timeout_sell_order_control_not_timed_out() {
+    new_test_ext().execute_with(|| {
+        let price: u128 = 5;
+        let secret = [0; 32];
+        let hashed_secret: [u8; 32] = keccak_256(&secret);
+        let _now = <pallet_timestamp::Pallet<Test>>::get();
+		assert_ok!(AcuityAtomicSwap::add_to_order(Origin::signed(A), price, 50));
+        assert_ok!(AcuityAtomicSwap::lock_sell(Origin::signed(A), price, hashed_secret, _now, 10));
+        assert_ok!(AcuityAtomicSwap::timeout_sell(Origin::signed(A), price, hashed_secret));
+	});
+}
+
+#[test]
+fn timeout_sell_order_fail_not_timed_out() {
+    new_test_ext().execute_with(|| {
+        let price: u128 = 5;
+        let secret = [0; 32];
+        let hashed_secret: [u8; 32] = keccak_256(&secret);
+        let _now = <pallet_timestamp::Pallet<Test>>::get();
+		assert_ok!(AcuityAtomicSwap::add_to_order(Origin::signed(A), price, 50));
+        assert_ok!(AcuityAtomicSwap::lock_sell(Origin::signed(A), price, hashed_secret, _now + 1000, 10));
+		assert_noop!(
+            AcuityAtomicSwap::timeout_sell(Origin::signed(A), price, hashed_secret),
+			Error::<Test>::LockNotTimedOut
+		);
+	});
+}
+
+#[test]
+fn timeout_sell() {
+	new_test_ext().execute_with(|| {
+        let price: u128 = 5;
+        let secret = [0; 32];
+        let hashed_secret: [u8; 32] = keccak_256(&secret);
+        let _now = <pallet_timestamp::Pallet<Test>>::get();
+		assert_ok!(AcuityAtomicSwap::add_to_order(Origin::signed(A), price, 50));
+
+        assert_eq!(Balances::free_balance(A), 50);
+        assert_eq!(Balances::free_balance(B), 200);
+        assert_eq!(Balances::free_balance(AcuityAtomicSwap::fund_account_id()), 50);
+
+        let order_id: [u8; 16] = blake2_128(&[A.to_ne_bytes().to_vec(), price.to_ne_bytes().to_vec()].concat());
+
+        assert_eq!(AcuityAtomicSwap::order_id_value(order_id), 50);
+
+        assert_ok!(AcuityAtomicSwap::lock_sell(Origin::signed(A), price, hashed_secret, _now, 10));
+        assert_eq!(AcuityAtomicSwap::order_id_value(order_id), 40);
+
+        let lock = AcuityAtomicSwap::sell_lock(hashed_secret);
+        assert_eq!(lock.order_id, order_id);
+        assert_eq!(lock.value, 10);
+        assert_eq!(lock.timeout, _now);
+
+        assert_ok!(AcuityAtomicSwap::timeout_sell(Origin::signed(A), price, hashed_secret));
+        assert_eq!(AcuityAtomicSwap::order_id_value(order_id), 50);
+
+        let lock = AcuityAtomicSwap::sell_lock(hashed_secret);
+        assert_eq!(lock.value, 0);
+        assert_eq!(lock.timeout, 0);
+
+        assert_eq!(Balances::free_balance(A), 50);
+        assert_eq!(Balances::free_balance(B), 200);
+        assert_eq!(Balances::free_balance(AcuityAtomicSwap::fund_account_id()), 50);
+
+	});
+}
