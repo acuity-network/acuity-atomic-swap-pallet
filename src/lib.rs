d0ccc1c09ae5ad3934ef8aac43d1b568f27bb9c6
Jonathan Brown <jbrown@acuity.social>
2022-07-26 23:15:22 +0700

    Declare event indexing storage.

--- src/lib.rs
+++ src/lib.rs
@@ -333,6 +333,28 @@ pub mod pallet {
         BalanceOf<T>, ValueQuery
     >;
 
+    #[pallet::storage]
+    #[pallet::getter(fn account_start_index)]
+    pub(super) type AccountStartIndex<T: Config> = StorageMap<_,
+        Blake2_128Concat, T::AccountId,
+        u64, ValueQuery
+    >;
+
+    #[pallet::storage]
+    #[pallet::getter(fn account_next_index)]
+    pub(super) type AccountNextIndex<T: Config> = StorageMap<_,
+        Blake2_128Concat, T::AccountId,
+        u64, ValueQuery
+    >;
+
+    #[pallet::storage]
+    #[pallet::getter(fn account_index_height)]
+    pub(super) type AccountIndexHeight<T: Config> = StorageDoubleMap<_,
+        Blake2_128Concat, T::AccountId,
+        Blake2_128Concat, u64,
+        u64
+    >;
+
     impl<T: Config> Pallet<T> {
     	/// The account ID of the fund pot.
     	///
